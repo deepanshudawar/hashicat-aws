5a7591507db7563f06a88942aa076714cd67d503
deepanshudawar <get2deepanshu@gmail.com>
2022-03-27 23:25:46 +0530

    test

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
  source  = "app.terraform.io/hashicorpdeepanshu/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucketsadsad123"
   acl    = "private"
  version="2.8.0"
 
